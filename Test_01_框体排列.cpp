2125cec5cd5cd0cbbe98e72a4e820a82635fdb8b
MZ_Benjamin <ben199709@foxmail.com>
2022-10-26 14:54:00 +0800

    First Problem

---
+++ Test_01_框体排列.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+int main()
+{
+	int amount = 0, width = 0, count = 0;
+	cin >> amount >> width;
+	vector<int> Numbers;
+	int toNumber=0;
+	while (cin>>toNumber)
+	{
+		Numbers.push_back(toNumber);
+		if (cin.get() == '\n')
+		{
+			break;
+		}
+	};
+	sort(Numbers.begin(), Numbers.end());//≈≈–Ú
+	int tempValue = Numbers[0];
+	if (Numbers.size() == 0)
+	{
+		cout << count;
+		return 0;
+	}
+	++count;
+	for (int i=0; i<Numbers.size(); ++i)
+	{
+		if (Numbers[i] > tempValue + width)
+		{
+			++count;
+			tempValue = Numbers[i];
+		}
+	}
+	cout << count;
+	return 0;
+}
