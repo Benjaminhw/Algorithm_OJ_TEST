6dad33ae58ae6435d82440fd2a346b36d76a86ab
benjamin <ben199709@foxmail.com>
2022-10-26 23:10:37 +0800

    3rd problem

---
+++ Test_03_构造大数组证明选择排序的优劣/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int number = 120000;
+	cout << number << endl;
+	for (int i = number; i >0 ; --i)
+	{
+		printf ("%d ",i);
+	}
+	return 0;
+}
