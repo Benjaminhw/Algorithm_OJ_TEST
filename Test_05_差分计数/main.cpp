d69267f0e7e1582742834bb9ceda13f2980c6e21
MZ_Benjamin <ben199709@foxmail.com>
2022-10-28 15:50:26 +0800

    5th Problem

--- Test_05_差分计数/main.cpp
+++ Test_05_差分计数/main.cpp
@@ -49,6 +49,16 @@ using namespace std;
 //	return;
 //}
 
+long long factorial(int n)
+{
+	long long result = 1;
+	for (int i = 1; i <= n; ++i)
+	{
+		result *= i;
+	}
+	return result;
+}
+
 int main()
 {
 	int Nouse = 0, width = 0;
@@ -84,6 +94,11 @@ int main()
 			break;
 		}
 	}
+	if (Nouse <= 1)
+	{
+		cout << 0;
+		return 0;
+	}
 
 	sort(Numbers.begin(), Numbers.end());
 	//for (int i = 0; i < Numbers.size() - 1; ++i)//最后一个不用找了
@@ -97,34 +112,62 @@ int main()
 	int x = 0, y = 1;
 	int size = Numbers.size();
 
-	while (x < size - 1 && y < size)
-	{
-		if (Numbers[y] < Numbers[x] + width)
+	if (width)
+	{//宽度不为0
+		while (x < size - 1 && y < size)
 		{
-			++y;
-		}
-		else if (Numbers[y] == Numbers[x] + width)
-		{
-			++count;
-			int a = x, b = y, flaga = Numbers[x], flagb = Numbers[y];
-			while (b < size-1 && Numbers[b + 1] == flagb)//相同的值
+			if (Numbers[y] < Numbers[x] + width)
 			{
-				++count;
-				++b;
+				++y;
 			}
-			//count += a * b;//没有相同就是1*1
-			++x;//判断完了 下一位
+			else if (Numbers[y] == Numbers[x] + width)
+			{
+
+				int a = 1, b = 1, flaga = Numbers[x], flagb = Numbers[y];
+				//下面这样写的好处是不走回头路，一步到底，复杂度为O(n) 只走两遍
+				while (x < size - 2 && Numbers[x + 1] == flaga)//x<y-1因为这两个数不可以重叠；而且必须先判断x再判断y
+				{
+					++a;
+					++x;
+				}
+				while (y < size - 1 && Numbers[y + 1] == flagb)//越界条件写前面避免访问越界错
+				{
+					++b;
+					++y;
+				}
+				count += a * b;//没有相同就是1*1
+				++x;//判断完了 下一位
+			}
+			else
+			{
+				++x;
+			}
 		}
-		else
+	}
+	else
+	{//宽度为0；
+		//注意这里并不需要判断是否接壤，当宽度是0的时候，x和y必然是始终接壤的。此时只需要一个指针
+		int flag = Numbers[x], N = 0;
+		while (x < size)
 		{
-			++x;
+			if (x < size - 1 && Numbers[x + 1] == flag)
+			{//如果后一个元素与前一个元素相同
+				++x;
+				++N;
+			}
+			else
+			{//不同，或是到达了数组的末端
+				count += N*(N+1);//计数加上这一堆数的阶乘
+				N = 0;
+				++x;
+				if (x < size)
+				{//没有到达末端
+					flag = Numbers[x];
+				}
+			}
 		}
 	}
 
-	if (width == 0)//如果宽度是0，那么需要乘2，因为只统计了一半
-	{
-		count *= 2;
-	}
 	cout << count;
 	return 0;
 }
