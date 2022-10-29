8bc7b8f854dc257bafdc2b4c6ceac36c17742b0c
MZ_Benjamin <ben199709@foxmail.com>
2022-10-29 16:29:56 +0800

    6th Problem

--- Test_06_制作菜肴/main.cpp
+++ Test_06_制作菜肴/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-#include <unordered_map>
+#include <map>
 
 using namespace std;
 
@@ -23,7 +23,7 @@ int main()
 {
 	int width = 0, height = 0, tempInt = 0;
 	//并不是单纯的排序，而需要排序且把排序前的坐标全部保存下来
-	unordered_map<pMyset, int> Garden;
+	map<pMyset, int> Garden;
 	cin >> height >> width;
 	while (getchar() != '\n') {};
 	for (int i = 1; i <= height; ++i)
@@ -52,46 +52,53 @@ int main()
 		{//现在是要挑选小明的站位
 			if (i == 1 || j == 1 || i == height || j == width)
 			{
-				printf("0 ");
+				if (j == width)
+				{
+					printf("0");
+				}
+				else
+				{
+					printf("0 ");
+				}
 			}
 			else
 			{
 				int a = 0, b = 0, c = 0, d = 0;//左上 右上 左下 右下
 				bool a1 = true, b1 = true, c1 = true, d1 = true;
-				for (vector<pair<pMyset, int>>::iterator it = GardenSortQueue.begin(); (a1 || b1 || c1 || d1 ) & it != GardenSortQueue.end(); ++it)
+				for (vector<pair<pMyset, int>>::iterator it = GardenSortQueue.begin(); (a1 || b1 || c1 || d1 ) && it != GardenSortQueue.end(); ++it)
 				{
 					if (it->first->i < i)
-					{//左半区
+					{//上半区
 						if (it->first->j > j && a1)
-						{//左上
+						{//上右
 							a1 = false;
 							a = it->second;
 						}
 						if (it->first->j < j && c1)
-						{//左下
+						{//上左
 							c1 = false;
 							c = it->second;
 						}
 					}
 					if (it->first->i > i)
-					{//右半区
+					{//下半区
 						if (it->first->j > j && b1)
-						{//右上
+						{//下右
 							b1 = false;
 							b = it->second;
 						}
 						if (it->first->j < j && d1)
-						{//右下
+						{//下左
 							d1 = false;
 							d = it->second;
 						}
 					}
 				}
-				cout << a * b * c * d << ' ';
+				unsigned long x = (unsigned long)a * b * c * d;
+				cout << x << ' ';
 			}
 		}
 		printf("\n");
 	}
-
 	return 0;
 }
